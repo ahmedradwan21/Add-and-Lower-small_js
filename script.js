1e36d96a770a1260cd974d06c815b717f6317759
Radwan <tara02664@gmail.com>
2023-07-08 03:59:37 +0300

    'ATR'

---
+++ script.js
@@ -0,0 +1,29 @@
+(function(){
+    const buttons = document.querySelectorAll('.counterBtn')
+    let count = 0
+    buttons.forEach(function(button) {
+        button.addEventListener('click',function(){
+            
+            if(button.classList.contains('prevBtn')){
+                count--
+            }
+            else if(button.classList.contains('nexBtn')){
+                count++
+            }
+            const counter = document.querySelector('#counter')
+            counter.textContent = count
+
+            if(count < 0){
+                counter.style.color = 'red'
+            }
+            else if(count > 0)
+            {
+                counter.style.color = 'green'
+            }
+            else
+            {
+                counter.style.color = 'blue'
+            }
+        })
+    });
+}) ()
